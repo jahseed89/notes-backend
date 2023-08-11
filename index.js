a7df6736bcd584fba76c290058dbb9bde6e2b5ef
Samson Ocran <samson.ocran@gmail.com>
2023-08-11 10:56:51 +0100

    mongoDB generated

--- index.js
+++ index.js
@@ -1,120 +1,73 @@
 const express = require('express')
-const dotenv = require('dotenv')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
 
-dotenv.config()
+const Note = require('./models/note')
 
 const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-  }
-  
-  const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-  }
-  
-  app.use(cors())
-  app.use(express.json())
-  app.use(requestLogger)
-  app.use(express.static('build'))
-  
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  },
-  {
-    id: 4,
-    content: "Learning backend is not a small jurney",
-    important: true
-  },
-  {
-    id: 5,
-    content: "We are happy to code",
-    important: true
-  },
-  {
-    id: 6,
-    content: "Ahh' Deployment is working fine now",
-    important: true
-  }
-]
-
-app.get('/', (req, res) => {
-  res.send('<h1>Welcome to backend!</h1>')
-})
-
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
-})
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
 }
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(cors())
+app.use(express.json())
+app.use(requestLogger)
+app.use(express.static('build'))
+
+let notes = []
+
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes);
+  }).catch(error => {
+    response.status(500).json({ error: 'Internal server error' });
+  });
+});
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
-
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
-})
+  Note.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch(error => {
+      response.status(500).json({ error: 'Internal server error' });
+    });
+});
 
 app.use(unknownEndpoint)
 
 const PORT = process.env.PORT
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
