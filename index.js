4b17b0d4c3a964c8ffa392bf564c0ce87736a825
Samson Ocran <samson.ocran@gmail.com>
2023-08-22 12:48:49 +0100

    resulved chore

--- index.js
+++ index.js
@@ -1,73 +1,81 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
-const Note = require('./models/note')
+const Note = require("./models/note");
 
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
-app.use(cors())
-app.use(express.json())
-app.use(requestLogger)
-app.use(express.static('build'))
+app.use(cors());
+app.use(express.json());
+app.use(requestLogger);
+app.use(express.static("build"));
 
-let notes = []
+let notes = [];
 
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes);
-  }).catch(error => {
-    response.status(500).json({ error: 'Internal server error' });
-  });
+app.get("/api/notes", (request, response) => {
+  Note.find({})
+    .then((notes) => {
+      response.json(notes);
+    })
+    .catch((error) => {
+      response.status(500).json({ error: "Internal server error" });
+    });
 });
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
 
   if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' })
+    return response.status(400).json({ error: "content missing" });
   }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
-  })
+  });
 
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
-})
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
+});
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
-})
+app.get("/api/notes/:id", (request, response) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => next(error))
+});
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete("/api/notes/:id", (request, response) => {
   Note.findByIdAndRemove(request.params.id)
     .then(() => {
       response.status(204).end();
     })
-    .catch(error => {
-      response.status(500).json({ error: 'Internal server error' });
+    .catch((error) => {
+      response.status(500).json({ error: "Internal server error" });
     });
 });
 
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
